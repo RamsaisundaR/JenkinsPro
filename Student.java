a6ad7e7fc3660a2141abda089b20633dfe0c9393
RamsaisundaR <163972340+RamsaisundaR@users.noreply.github.com>
2026-03-22 09:33:35 +0530

    Change course name and remove duplicate output
    Updated the course name from 'PYTHON' to 'DEV-SEC-OPS' and removed duplicate roll number print statements.

--- Student.java
+++ Student.java
@@ -2,12 +2,11 @@ public class Student {
     public static void main(String[] args) {
         int rollno = 28035;
         String sname = "RAM";
-        String course = "PYTHON"; 
+        String course = "DEV-SEC-OPS"; 
         System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
-         System.out.println("My Roll Number is: " + rollno);
-         System.out.println("My Roll Number is: " + rollno);
+      
     }
 }
